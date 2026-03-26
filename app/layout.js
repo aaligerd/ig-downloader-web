ebe302fd998c3ff3922816e7a172b628c6400510
Subhradip Majumder <amerpoth@gmail.com>
2026-03-26 18:24:32 +0530

    adding gtm

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from '../components/Footer';
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +21,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* GA4 Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-27GR48OO5D"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+
+            gtag('js', new Date());
+
+            // ✅ disable auto page_view
+            gtag('config', 'G-27GR48OO5D', {
+              send_page_view: false
+            });
+          `}
+        </Script>
+      </head>
       <body>
         <div className="flex flex-col min-h-screen">
           <div className="grow">
--- app/page.js
+++ app/page.js
@@ -64,7 +64,15 @@ const jsonLd = {
 
 export default function Home() {
 
-
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('event', 'page_view', {
+        page_title: document.title,
+        page_location: window.location.href,
+        author_name: 'Subhradip Majumder'
+      });
+    }
+  }, []);
   return (
     <>
       <script
